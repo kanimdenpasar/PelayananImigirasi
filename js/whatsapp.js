dd6c3481223d90aa54dc10feafefe5af5ce6a445
kanimdenpasar <ekiputra543@gmail.com>
2025-10-23 20:38:43 +0700

    feat(main) : update number

--- js/whatsapp.js
+++ js/whatsapp.js
@@ -1,15 +1,15 @@
 const WHATSAPP_CONFIG = {
   satgas: {
-    number: "6281234567890", // Replace with actual Satgas Patroli number
+    number: "6281927496534", // Replace with actual Satgas Patroli number
     message: "Halo, saya ingin menghubungi Satgas Patroli Imigrasi Denpasar.",
   },
   pengaduan: {
-    number: "6281234567891", // Replace with actual Layanan Pengaduan number
+    number: "6281927496534", // Replace with actual Layanan Pengaduan number
     message:
       "Halo, saya ingin melaporkan pengaduan ke Kantor Imigrasi Kelas I Denpasar. Berikut adalah format laporan pengaduan saya:\n\nNama: [Nama Lengkap]\nEmail: [Email]\nNomor Telepon: [Nomor]\nSubjek: [Subjek Pengaduan]\nIsi Pengaduan: [Isi Pengaduan]",
   },
   complaint: {
-    number: "6285794242709", // Nomor untuk form pengaduan
+    number: "6281927496534", // Nomor untuk form pengaduan
   },
 }
 
